148e8a4d59e8e67f63c93490d1cc84b83933cb76
Роман Давыденко <ishadows888@gmail.com>
2020-11-12 20:56:14 +0700

    Добавил интерфейс логгера и его реализацию с помощью Serilog

---
+++ Snt22Progress.Logging/IProgressLogger.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snt22Progress.Logging
+{
+	/// <summary>
+	/// Интерфейс логгера
+	/// </summary>
+	public interface IProgressLogger
+	{
+		/// <summary>
+		/// Запись лога уровня "Информация"
+		/// </summary>
+		/// <param name="message">Сообщение</param>
+		/// <param name="src">Объект логгирования</param>
+		/// <param name="className">Имя класса</param>
+		/// <param name="methodName">Имя метода</param>
+		public void Information(string message, object src = null, string className = null, string methodName = null);
+
+		/// <summary>
+		/// Запись лога уровня "Опасность"
+		/// </summary>
+		/// <param name="message">Сообщение</param>
+		/// <param name="src">Объект логгирования</param>
+		/// <param name="className">Имя класса</param>
+		/// <param name="methodName">Имя метода</param>
+		public void Warning(string message, object src = null, string className = null, string methodName = null);
+
+		/// <summary>
+		/// Запись лога уровня "Опасность"
+		/// </summary>
+		/// <param name="ex">Исключение</param>
+		/// <param name="src">Объект логгирования</param>
+		/// <param name="className">Имя класса</param>
+		/// <param name="methodName">Имя метода</param>
+		public void Warning(Exception ex, object src = null, string className = null, string methodName = null);
+
+		/// <summary>
+		/// Запись лога уровня "Ошибка"
+		/// </summary>
+		/// <param name="message">Сообщение</param>
+		/// <param name="src">Объект логгирования</param>
+		/// <param name="className">Имя класса</param>
+		/// <param name="methodName">Имя метода</param>
+		public void Error(string message, object src = null, string className = null, string methodName = null);
+
+		/// <summary>
+		/// Запись лога уровня "Ошибка"
+		/// </summary>
+		/// <param name="ex">Исключение</param>
+		/// <param name="src">Объект логгирования</param>
+		/// <param name="className">Имя класса</param>
+		/// <param name="methodName">Имя метода</param>
+		public void Error(Exception ex, object src = null, string className = null, string methodName = null);
+
+		/// <summary>
+		/// Запись лога уровня "Фатальная ошибка"
+		/// </summary>
+		/// <param name="message">Сообщение</param>
+		/// <param name="src">Объект логгирования</param>
+		/// <param name="className">Имя класса</param>
+		/// <param name="methodName">Имя метода</param>
+		public void Fatal(string message, object src = null, string className = null, string methodName = null);
+
+		/// <summary>
+		/// Запись лога уровня "Фатальная ошибка"
+		/// </summary>
+		/// <param name="ex">Исключение</param>
+		/// <param name="src">Объект логгирования</param>
+		/// <param name="className">Имя класса</param>
+		/// <param name="methodName">Имя метода</param>
+		public void Fatal(Exception ex, object src = null, string className = null, string methodName = null);
+	}
+}
---
+++ Snt22Progress.Logging/ProgressLogger.cs
@@ -0,0 +1,68 @@
+﻿using Serilog;
+using Serilog.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Snt22Progress.Logging
+{
+	/// <summary>
+	/// Реализация логгера
+	/// </summary>
+	public class ProgressLogger : IProgressLogger
+	{
+		protected readonly string _folderName;
+
+		protected readonly ILogger _logger;
+
+		public string DirectoryPath => Path.Combine(Directory.GetCurrentDirectory(), _folderName);
+
+		public ProgressLogger(string folderName = "logs")
+		{
+			_folderName = folderName;
+
+			var configuration = new LoggerConfiguration();
+			configuration.WriteTo.File(
+				path: DirectoryPath				
+				);
+
+			_logger = configuration.CreateLogger();
+		}
+
+		public void Information(string message, object src = null, string className = null, string methodName = null)
+		{
+			_logger.Information(message, src, className, methodName);
+		}
+
+		public void Warning(string message, object src = null, string className = null, string methodName = null)
+		{
+			_logger.Warning(message, src, className, methodName);
+		}
+
+		public void Warning(Exception ex, object src = null, string className = null, string methodName = null)
+		{
+			_logger.Warning(ex, ex.Message, src, className, methodName);
+		}
+
+		public void Error(string message, object src = null, string className = null, string methodName = null)
+		{
+			_logger.Error(message, src, className, methodName);
+		}
+
+		public void Error(Exception ex, object src = null, string className = null, string methodName = null)
+		{
+			_logger.Error(ex, ex.Message, src, className, methodName);
+		}
+
+		public void Fatal(string message, object src = null, string className = null, string methodName = null)
+		{
+			_logger.Fatal(message, src, className, methodName);
+		}
+
+		public void Fatal(Exception ex, object src = null, string className = null, string methodName = null)
+		{
+			_logger.Fatal(ex, ex.Message, src, className, methodName);
+		}
+	}
+}
